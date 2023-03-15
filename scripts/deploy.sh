5df17cbe7e8214fc42dc423305f43ead02278f8a
hkh <dkf3rnl@gmail.com>
2023-03-15 20:08:57 +0900

    deploy.sh수정2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,6 +26,7 @@ echo "> $JAR_NAME에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-nohup java -jar $JAR_NAME \
+nohup java -jar \
         -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
-        -Dspring.profiles.active=real > $REPOSITORY/nohup.out 2>&1 &
+        -Dspring.profiles.active=real \
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
